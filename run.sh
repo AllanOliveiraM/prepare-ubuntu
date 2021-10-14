bbaed0f88d069f327e70ab64d2c80b0a0183ded7
Allan Oliveira <nallamo2016@gmail.com>
2021-10-14 00:10:32 -0300

    removed unused code

--- run.sh
+++ run.sh
@@ -6,3 +6,9 @@ usermod -s /usr/bin/zsh $(whoami)
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+
+echo "
+Run 'nano ~/.zshrc' and add append zsh-autosuggestions & zsh-syntax-highlighting to plugins() like this:
+
+plugins=(git zsh-autosuggestions zsh-syntax-highlighting)
+"
